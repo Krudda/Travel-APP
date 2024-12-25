55632c55ce31546ab0c2fe4b423c874ce47ec5cd
krudda <krudda@yandex.ru>
2024-12-25 23:49:29 +0300

    Add streaming generated object

--- ai-travel-app/README.md
+++ ai-travel-app/README.md
@@ -1,9 +1,8 @@
-
 ## AI Travel App
 
 1. Create Next.js app: npx create-next-app@latest ai-travel-app
-   - Run app: npm run dev
-   - Open: [http://localhost:3000](http://localhost:3000)
+    - Run app: npm run dev
+    - Open: [http://localhost:3000](http://localhost:3000)
 
 2. Install ia packages: npm i ai @ai-sdk/openai
 3. Create API keys: https://platform.openai.com/
@@ -15,4 +14,5 @@
 9. Add actions.js file with generateText async function
 10. Replace generateText function in actions with generate function, contains createStreamableValue call
 11. Add zod package
+12. Add streaming generated object
 
--- ai-travel-app/src/app/actions.js
+++ ai-travel-app/src/app/actions.js
@@ -1,24 +1,36 @@
 'use server';
 
-import { generateObject } from 'ai';
 import { openai } from '@ai-sdk/openai';
+import { streamObject } from 'ai';
 import { z } from 'zod';
+import { createStreamableValue } from 'ai/rsc';
 
-export async function getData(input) {
+export async function generate(input) {
   'use server';
-  const { object: people } = generateObject({
-    model: openai('gpt-4-turbo'),
-    system: 'You generate fake data for three people',
-    prompt: input,
-    schema: z.object({
-      people: z.array(
-        z.object({
-          name: z.string().describe('name of a fake person'),
-          address: z.string().describe('US adresses format'),
-          age: z.number(),
-        }),
-      ),
-    }),
-  });
-  return { people };
+
+  const stream = createStreamableValue();
+
+  (async () => {
+    const { partialObjectStream } = streamObject({
+      model: openai('gpt-4-turbo'),
+      system: 'You generate fake data for three people',
+      prompt: input,
+      schema: z.object({
+        people: z.array(
+          z.object({
+            name: z.string().describe('name of a fake person'),
+            address: z.string().describe('US adresses format'),
+            age: z.number(),
+          }),
+        ),
+      }),
+    });
+
+    for await (const partialObject of partialObjectStream) {
+      stream.update(partialObject);
+    }
+    stream.done();
+  })();
+
+  return { object: stream.value };
 }
